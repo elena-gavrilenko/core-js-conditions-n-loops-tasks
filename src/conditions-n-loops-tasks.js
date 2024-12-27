5ea2e8dee1d9ddd4aed89629c83cf2116271ea1f
Helen <gomel-offis@mail.ru>
2024-12-27 18:06:18 +0300

    fix: fix task rotateMatrix

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -405,14 +405,11 @@ function rotateMatrix(mat) {
   const matrix = mat;
   const n = matrix.length;
 
-  // First transpose the matrix
   for (let i = 0; i < n; i += 1) {
     for (let j = i; j < n; j += 1) {
       [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
     }
   }
-
-  // Then reverse each row
   for (let i = 0; i < n; i += 1) {
     for (let j = 0; j < n / 2; j += 1) {
       [matrix[i][j], matrix[i][n - 1 - j]] = [
